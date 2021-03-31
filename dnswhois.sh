097f31a2d3970c0b100dd3b16a655f540ca476f3
Matt <matthew.a.torrisi@gmail.com>
2021-03-30 20:27:37 -0400

    Uploaded code base

---
+++ dnswhois.sh
@@ -0,0 +1,10 @@
+printf "\n"; 
+read -p "Enter domain name.." domain; 
+tldns=$(dig $(echo $domain | sed 's/.*\.//') ns +short | sed -n 2p); 
+ns=$(dig @$tldns $domain +short); 
+ns+=$(dig +noall @$tldns $domain ns +authority | awk '{print$5}'); 
+printf "\nDELEGATION FOR - $domain \n"; for i in ${ns[@]}; 
+do printf "\nFQDN: $i - "; 
+ip=$(dig $i +short); printf "$ip\n"; 
+whois $ip | awk '/Organization/ || /NetRange/ || /descr/ {print$0}'; 
+done; printf "\n";
