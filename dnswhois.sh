12fa08ef6d8654390341f08b5ab3e863ed606fcc
Matt <matthew.a.torrisi@gmail.com>
2021-07-27 15:13:26 -0400

    Update dnswhois.sh

--- dnswhois.sh
+++ dnswhois.sh
@@ -1,10 +1,13 @@
-printf "\n"; 
-read -p "Enter domain name.." domain; 
+#!/usr/bin/env bash
+
+domain=$1
 tldns=$(dig $(echo $domain | sed 's/.*\.//') ns +short | sed -n 2p); 
 ns=$(dig @$tldns $domain +short); 
 ns+=$(dig +noall @$tldns $domain ns +authority | awk '{print$5}'); 
-printf "\nDELEGATION FOR - $domain \n"; for i in ${ns[@]}; 
-do printf "\nFQDN: $i - "; 
-ip=$(dig $i +short); printf "$ip\n"; 
-whois $ip | awk '/Organization/ || /NetRange/ || /descr/ {print$0}'; 
-done; printf "\n";
+printf "\nDELEGATION FOR - $domain \n"; 
+for i in ${ns[@]}; 
+  do printf "\nFQDN: $i - "; 
+  ip=$(dig $i +short); printf "$ip\n"; 
+  whois $ip | awk '/Organization/ || /NetRange/ || /descr/ {print$0}'; 
+  done; 
+printf "\n";
